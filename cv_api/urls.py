8f4599ff1a611207e46bbca381922449dffa6bdb
Dario Velez Roman <dario.velez@dinmax.com>
2019-07-18 00:33:28 -0300

    api cv

--- cv_api/urls.py
+++ cv_api/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+                       # Examples:
+                       url(r'^face_detection/detect/$',
+                           'face_detector.views.detect'),
+
+                       # url(r'^$', 'cv_api.views.home', name = 'home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
--- face_detector/views.py
+++ face_detector/views.py
@@ -5,6 +5,11 @@ import numpy as np
 import urllib
 import json
 import cv2
+import os
+
+# define the path to the face detector
+FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
+    base_path=os.path.abspath(os.path.dirname(__file__)))
 
 
 @csrf_exempt
@@ -37,16 +42,18 @@ def detect(request):
         # and detect faces in the image
         image = cv2.cvtColor(image, cv2.COLOR_BGR2BGR555)
         detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
-        rects = detector.detectMultiScale(image, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+        rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(
+            30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
         # construct a list of bounding boxes from the detection
-        rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
+        rects = [(int(x), int(y), int(x + w), int(y + h))
+                 for (x, y, w, h) in rects]
 
         # update the data dictionary with the faces detected
-        data.update({"num_faces": len(rects), "faces": rects, "success": True })
+        data.update({"num_faces": len(rects), "faces": rects, "success": True})
 
         # return a JSON response
-        return JsonResponse(data) 
+        return JsonResponse(data)
         # END WRAPPING OF COMPUTER VISION APP
 
         # update the data dictionary
@@ -54,3 +61,27 @@ def detect(request):
 
     # return a JSON response
     return JsonResponse(data)
+
+    def _grab_image(path=None, stream=None, url=None):
+        # if the path is not None, then load the image from disk
+        if path is not None:
+            image = cv2.imread(path)
+
+        # otherwise, the image does not reside in disk
+        else:
+            # if the URL is not None, then download the image
+            if url is not None:
+                resp = urllib.urllopen(url)
+                data = resp.read()
+
+            # if the stream is not None, then the image has been uploaded
+            elif stream is not None:
+                data = stream.read()
+
+            # convert the image to a Numpy array and then read it into
+            # OpenCV format
+            image = np.asarray(bytearray(data), dtype="uint8")
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        # return the image
+        return image
